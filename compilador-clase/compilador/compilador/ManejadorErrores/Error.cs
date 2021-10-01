6da8581edff17eb8d2f884a40fd94b1c22a7bbc6
andersonpazotalvaro <69700905+andersonpazotalvaro@users.noreply.github.com>
2021-09-30 19:45:33 -0500

    30/09

---
+++ compilador-clase/compilador/compilador/ManejadorErrores/Error.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compilador.ManejadorErrores
+{
+    public class Error
+    {
+        private int NumeroLinea;
+        private int PosicionInicial;
+        private int PosicionFinal;
+        private string falla;
+        private string causa;
+        private string solucion;
+        private TipoError Tipo;
+
+        private Error(int NumeroLinea, int PosicionInicial, int PosicionFinal, string Falla, string Causa, string Solucion, TipoError Tipo)
+        {
+            this.NumeroLinea = NumeroLinea;
+            this.PosicionInicial = PosicionInicial;
+            this.PosicionFinal = PosicionFinal;
+            this.causa = Causa;
+            this.falla = Falla;
+            this.Tipo = Tipo;
+        }
+        public static Error Crear(int NumeroLinea,
+         int PosicionInicial,
+         int PosicionFinal,
+         string falla,
+         string causa,
+         string solucion,
+         TipoError Tipo)
+        {
+            return new Error(NumeroLinea, PosicionInicial, PosicionFinal, falla, causa, solucion, Tipo);
+         }
+        public int ObtenerNumeroLinea()
+        {
+            return NumeroLinea;
+        }
+
+        public int ObtenerPosicionInicial()
+        {
+            return PosicionInicial;
+        }
+
+        public int ObtenerPosicionFinal()
+        {
+            return PosicionFinal;
+        }
+
+        public string ObtenerFalla()
+        {
+            return falla;
+        }
+
+        public string ObtenerCausa()
+        {
+            return causa;
+        }
+
+        public string ObtenerSoucion()
+        {
+            return solucion;
+        }
+
+        public TipoError ObtenerTipo()
+        {
+            return Tipo;
+        }
+    }
+}
---
+++ compilador-clase/compilador/compilador/ManejadorErrores/GestorErrores.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compilador.ManejadorErrores
+{
+    class GestorErrores
+    {
+        private Dictionary<TipoError, List<Error>> Tabla = new Dictionary<TipoError, List<Error>>();
+        private static GestorErrores INSTANCIA = new GestorErrores();
+
+        private GestorErrores()
+        {
+            Iniciar();
+        }
+
+        public static GestorErrores ObtenerInstancia()
+        {
+            return INSTANCIA;
+        }
+
+        private void Iniciar()
+        {
+            Tabla.Clear();
+            Tabla.Add(TipoError.LEXICO, new List<Error>());
+            Tabla.Add(TipoError.SINTACTICO, new List<Error>());
+            Tabla.Add(TipoError.SEMANTICO, new List<Error>());
+        }
+        public void Limpiar()
+        {
+            Iniciar();
+        }
+
+        
+
+        public void Agregar(Error Error)
+        {
+            if (Error != null )
+            {
+                Tabla[Error.ObtenerTipo()].Add(Error);
+            }
+        }
+
+        public bool HayErrores(TipoError Tipo)
+        {
+            return Tabla[Tipo].Count > 0;
+        }
+
+        public bool HayErrores()
+        {
+            return HayErrores(TipoError.LEXICO) || HayErrores(TipoError.SEMANTICO) || HayErrores(TipoError.SINTACTICO);
+        }
+
+        public List<Error> ObtenerErrores(TipoError Tipo)
+        {
+            return Tabla[Tipo];
+        }
+    }
+}
